02c59bd1dcdc3b2ca70a09beda65c0504ea8a3b7
kushurox <38589424+kushurox@users.noreply.github.com>
2018-05-14 19:07:55 +0530

    Update KushBot.py

--- KushBot.py
+++ KushBot.py
@@ -95,7 +95,7 @@ async def on_member_remove(member: discord.Member):
     await client.send_message(serverchannel, embed=embl)
 
 @client.command(pass_context=True)
-async def timer(ctx, units = "none", amount :int = -1, *, reason):
+async def timer(ctx, units = "none", amount :int = -1, *, reason = " "):
     
     if amount == -1:
         await client.say("Invalid Argument try for k!help or k!cmds")
